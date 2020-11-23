11eedb9139a592eacc2be46adf9a0a478a31a612
pywd <yuki.wd7@gmail.com>
2020-11-23 16:11:02 +0900

    naruhodo~~

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <Head>
+        <title>morimo</title>
+      </Head>
+      <main>MORIMO🌳</main>
+    </div>
+  );
+}
