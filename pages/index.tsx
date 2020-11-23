dbd2d75d8bda0aa10657f405a66ea2f760d1ed19
pywd <yuki.wd7@gmail.com>
2020-11-23 18:00:04 +0900

    add basic layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
 import Head from "next/head";
+import styles from "../styles/home.module.css";
 
 export default function Home(): JSX.Element {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>morimo</title>
       </Head>
-      <main>MORIMO🌳</main>
+      <main>
+        <h1 className={styles.title}>🌳MORIMO🌳</h1>
+        <div className={styles.border} />
+        <section className={styles.articles}>
+          <ul>
+            <li>記事タイトル</li>
+            <li>記事タイトル</li>
+            <li>記事タイトル</li>
+            <li>記事タイトル</li>
+            <li>記事タイトル</li>
+            <li>記事タイトル</li>
+            <li>記事タイトル</li>
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
