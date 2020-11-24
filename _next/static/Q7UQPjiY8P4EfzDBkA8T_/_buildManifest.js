76533c2fcf0ab815f05f7b806ea1d8b7f17028e7
yuki-wd <yuki-wd@users.noreply.github.com>
2020-11-24 00:20:18 +0000

    deploy: d59809a0a37ab9372ba24171561538a6868d831b

---
+++ _next/static/Q7UQPjiY8P4EfzDBkA8T_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F731fd9a28ba53644fb9a.css","static\u002Fchunks\u002Fpages\u002Findex-4c79327df309b39fb604.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ad598b51f83a893d98b3.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
