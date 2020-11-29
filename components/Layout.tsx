26981b010b08e0caa88273d3793ee7cfed650078
pywd <yuki.wd7@gmail.com>
2020-11-29 15:40:56 +0900

    add date display to article detail, and home link

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import styles from "./layout.module.css";
+import Link from "next/link";
 
 interface LayoutProps {
   children?: React.ReactNode;
@@ -7,7 +8,11 @@ export default function Layout({ children }: LayoutProps): JSX.Element {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <h1 className={styles.title}>🌳MORIMO🌳</h1>
+        <h1 className={styles.title}>
+          <Link href={"/"}>
+            <a>🌳MORIMO🌳</a>
+          </Link>
+        </h1>
       </header>
       <main>{children}</main>
     </div>
--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -5,17 +5,26 @@ import { getArticleIds, getArticle } from "../../lib/articles";
 import { ParsedUrlQuery } from "querystring";
 
 interface DetailProps {
+  date: string | null;
   title: string;
   content: string;
 }
 
-export default function Detail({ title, content }: DetailProps): JSX.Element {
+export default function Detail({
+  title,
+  content,
+  date,
+}: DetailProps): JSX.Element {
   return (
     <Layout>
       <Head>
         <title>{title}</title>
       </Head>
       <article>
+        <div>
+          {date && <span>{date}</span>}
+          <h1>{title}</h1>
+        </div>
         <div dangerouslySetInnerHTML={{ __html: content }} />
       </article>
     </Layout>
@@ -44,6 +53,7 @@ export const getStaticProps: GetStaticProps<DetailProps, Query> = async ({
   const article = await getArticle((params as Query).id);
   return {
     props: {
+      date: article.date,
       title: article.title,
       content: article.content,
     },
