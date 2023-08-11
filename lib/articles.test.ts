0c57ddeab19a4dcafc25a7c500acc8af6841d287
m0r1m0 <yuki.wd7@gmail.com>
2023-08-11 17:20:23 +0900

    test: fix test

--- lib/articles.test.ts
+++ lib/articles.test.ts
@@ -1,11 +1,12 @@
 import "jest";
 import { getDate } from "./articles";
+import dayjs from "dayjs";
 
 describe("getDate", () => {
   test("ファイル名に日付が含まれる", () => {
     const fileName = "2020-11-28-test1.md";
     const result = getDate(fileName);
-    expect(result).toBe("2020年11月28日");
+    expect(result).toStrictEqual(dayjs("2020-11-28").toDate());
   });
   test("ファイル名に日付が含まれていない", () => {
     const fileName = "test1.md";
