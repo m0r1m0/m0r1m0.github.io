5fcccdac321d202ed8be19462ff0214c33aa3d27
morimo <yuki.wd7@gmail.com>
2023-08-11 15:38:47 +0900

    Revert "chore: bump next to 13 (#29)" (#30)
    This reverts commit 02ea21ba031d2ebefb2aee8b7c2411ea48915e5a.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,9 @@ export default function Layout({ children }: LayoutProps): JSX.Element {
     <div className={styles.container}>
       <header className={styles.header}>
         <h1 className={styles.title}>
-          <Link href={"/"}>🌳MORIMO🌳</Link>
+          <Link href={"/"}>
+            <a>🌳MORIMO🌳</a>
+          </Link>
         </h1>
       </header>
       <main>{children}</main>
