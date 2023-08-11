02ea21ba031d2ebefb2aee8b7c2411ea48915e5a
morimo <yuki.wd7@gmail.com>
2023-08-11 14:26:55 +0900

    chore: bump next to 13 (#29)
    * chore: update lockfileVersion to 2
    
    * chore: bump next to 13.4.13

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,8 +1,5 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
 
-declare global {
-  interface Window {
-    gtag: Gtag.Gtag;
-  }
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
